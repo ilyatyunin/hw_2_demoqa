896bee760777ffa08e93c7634a067d4b3672d7ff
is_tyunin <tomslavg96@gmail.com>
2022-09-18 23:31:33 +0300

    final_autotest

---
+++ src/test/java/com/demoqa/hw_2_demoqa.java
@@ -0,0 +1,69 @@
+package com.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class hw_2_demoqa {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+    @Test
+    void fillFormTest() {
+        String firstName = "Ilya";
+        String lastName = "Tyunin";
+        String userEmail = "is_tyunin@gmail.com";
+        String userNumber = "9876543210";
+        String year = "1996";
+        String url = "src/test/data/tree.jpg";
+        String currentAddress = "Moscow";
+
+        open("/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue(userNumber);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionByValue(String.valueOf(4));
+        $(".react-datepicker__year-select").selectOption(year);
+        $(".react-datepicker__day--007").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFile(new File(url));
+        $("#currentAddress").setValue(currentAddress);
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Haryana")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Karnal")).click();
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave((text(firstName)),
+                text(lastName),
+                text(userEmail),
+                text("Male"),
+                text(userNumber),
+                text("07 May,1996"),
+                text("Maths"),
+                text("Sports"),
+                text("tree.jpg"),
+                text(currentAddress),
+                text("Haryana Karnal"));
+
+        $("#closeLargeModal").click();
+    }
+}
