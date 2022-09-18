e810a4facbf1b82c0ebfb3e18f3f24fb47b4e959
is_tyunin <tomslavg96@gmail.com>
2022-09-18 17:58:58 +0300

    fix gitignore

---
+++ src/test/java/com/demoqa/TextBoxTest.java
@@ -0,0 +1,13 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTest {
+
+    @Test
+    void fillFormTest() {
+        open("");
+    }
+}
