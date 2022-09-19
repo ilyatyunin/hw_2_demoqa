05387a626d5ff89611c2bc83461b5f5f42f677f7
is_tyunin <tomslavg96@gmail.com>
2022-09-19 08:27:32 +0300

    changes after review

--- src/test/java/com/demoqa/RegistrationFormTest.java
+++ src/test/java/com/demoqa/RegistrationFormTest.java
@@ -4,13 +4,14 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import javax.security.auth.Subject;
 import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class hw_2_demoqa {
+public class RegistrationFormTest {
 
     @BeforeAll
     static void setUp() {
@@ -20,50 +21,65 @@ public class hw_2_demoqa {
     }
     @Test
     void fillFormTest() {
+        // Ввод переменных
         String firstName = "Ilya";
         String lastName = "Tyunin";
         String userEmail = "is_tyunin@gmail.com";
+        String genter = "Male";
         String userNumber = "9876543210";
         String year = "1996";
-        String url = "src/test/data/tree.jpg";
+        String month = "May";
+        String dayOfMonth = "07";
+        String subjects = "Maths";
+        String hobbies = "Sports";
+        String dir = "src/test/resources/";
+        String file = "tree.jpg";
         String currentAddress = "Moscow";
+        String state = "Haryana";
+        String city = "Karnal";
 
+        // Открытие страницы
         open("/automation-practice-form");
         executeJavaScript("$('footer').remove()");
         executeJavaScript("$('#fixedban').remove()");
 
+        // Заполнение формы регистрации
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(userEmail);
-        $("#genterWrapper").$(byText("Male")).click();
+        $("#genterWrapper").$(byText(genter)).click();
         $("#userNumber").setValue(userNumber);
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOptionByValue(String.valueOf(4));
+        $(".react-datepicker__month-select").selectOption(month);
         $(".react-datepicker__year-select").selectOption(year);
-        $(".react-datepicker__day--007").click();
-        $("#subjectsInput").setValue("Math").pressEnter();
-        $("#hobbiesWrapper").$(byText("Sports")).click();
-        $("#uploadPicture").uploadFile(new File(url));
+        $(".react-datepicker__day--0" + dayOfMonth).click();
+        $("#subjectsInput").setValue(subjects).pressEnter();
+        $("#hobbiesWrapper").$(byText(hobbies)).click();
+        $("#uploadPicture").uploadFile(new File(dir + file));
         $("#currentAddress").setValue(currentAddress);
         $("#state").click();
-        $("#stateCity-wrapper").$(byText("Haryana")).click();
+        $("#stateCity-wrapper").$(byText(state)).click();
         $("#city").click();
-        $("#stateCity-wrapper").$(byText("Karnal")).click();
+        $("#stateCity-wrapper").$(byText(city)).click();
 
         $("#submit").click();
 
+        // Проверка заполненных полей
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
         $(".table-responsive").shouldHave((text(firstName)),
                 text(lastName),
                 text(userEmail),
-                text("Male"),
+                text(genter),
                 text(userNumber),
-                text("07 May,1996"),
-                text("Maths"),
-                text("Sports"),
-                text("tree.jpg"),
+                text(dayOfMonth),
+                text(month),
+                text(year),
+                text(subjects),
+                text(hobbies),
+                text(file),
                 text(currentAddress),
-                text("Haryana Karnal"));
+                text(state),
+                text(city));
 
         $("#closeLargeModal").click();
     }
